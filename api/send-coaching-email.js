5db7c0a5ffd11e6e4cd35da710d4ddfe7f1a2960
DeodathVR <deodathramkumar@yahoo.com>
2025-06-30 17:17:55 -0400

    Update send-coaching-email.js

--- api/send-coaching-email.js
+++ api/send-coaching-email.js
@@ -82,7 +82,7 @@ export default async function handler(req, res) {
     </html>
     `;
 
-    // Send email using SendGrid Web API
+    // Simplified email data - HTML only
     const emailData = {
       personalizations: [
         {
@@ -95,10 +95,6 @@ export default async function handler(req, res) {
         name: 'Your Habit Coach'
       },
       content: [
-        {
-          type: 'text/plain',
-          value: `Hi ${userName}, I noticed you haven't checked in for ${inactiveDays} days. Your ${longestStreak}-day streak shows real dedication! Visit https://www.myawesomelifehabits.com to continue your journey.`
-        },
         {
           type: 'text/html',
           value: htmlContent
