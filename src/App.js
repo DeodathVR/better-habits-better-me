123ae437b3325e3a9f6e37acd31916e5883b03f2
DeodathVR <deodathramkumar@yahoo.com>
2025-06-30 23:32:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
 
   const [currentUser, setCurrentUser] = useState({
     name: "Alex",
-    email: "sparksofmotivation1001@gmail.com",
+    email: "deodathramkumar@yahoo.com",
     phone: "+1 (555) 123-4567",
     isPremium: true,
     lastActiveDate: new Date('2025-06-11'),
